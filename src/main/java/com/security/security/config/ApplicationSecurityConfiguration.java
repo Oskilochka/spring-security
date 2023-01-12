05825d526eb128f5f378844faf3e266ac0d55e5d
Julia Oskilko <yulia.osk14@gmail.com>
2023-01-12 20:42:40 +0200

    add permissions for requests

--- src/main/java/com/security/security/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/security/security/config/ApplicationSecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.security.security.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
+import static com.security.security.config.ApplicationUserPermission.COURSE_WRITE;
 import static com.security.security.config.ApplicationUserRole.*;
 import static org.springframework.security.config.Customizer.withDefaults;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
@@ -30,7 +32,11 @@ public class ApplicationSecurityConfiguration {
 
         http.csrf().disable().authorizeHttpRequests(auth -> auth
                         .requestMatchers("").permitAll()
-                        .requestMatchers("/api/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                        .requestMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                        .requestMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.name())
+                        .requestMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+                        .requestMatchers("/api/**").hasRole(ADMIN.name())
                         .anyRequest()
                         .authenticated()
                 )
